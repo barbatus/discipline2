3ea1f8df95d87d3cd5cff9a9e4f2103878ee0a2f
Aliaksandr <alex.borodach@gmail.com>
2023-04-07 20:53:27 +0200

    Upgrade to RN 0.70 Fix Fastlane for Android

--- ios/Discipline/AppDelegate.m
+++ ios/Discipline/AppDelegate.m
@@ -48,6 +48,7 @@ RCTAppSetupPrepareApp(application);
   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
 #endif
 
+  NSDictionary *initProps = [self prepareInitialProps];
   RCTRootView *rootView = (RCTRootView *)RCTAppSetupDefaultRootView(bridge, @"Discipline", nil);
 
   if (@available(iOS 13.0, *)) {
@@ -72,6 +73,25 @@ RCTAppSetupPrepareApp(application);
   [GMSServices provideAPIKey:@"AIzaSyDnCjT2geaw4fX67TdxTc0db8X1djs1TdE"];
 
   return YES;
+}
+
+/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
+///
+/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
+/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
+/// @return: `true` if the `concurrentRoot` feture is enabled. Otherwise, it returns `false`.
+- (BOOL)concurrentRootEnabled
+{
+  // Switch this bool to turn on and off the concurrent root
+  return true;
+}
+- (NSDictionary *)prepareInitialProps
+{
+  NSMutableDictionary *initProps = [NSMutableDictionary new];
+#ifdef RCT_NEW_ARCH_ENABLED
+  initProps[kRNConcurrentRoot] = @([self concurrentRootEnabled]);
+#endif
+  return initProps;
 }
 
 // Required for the register event.
