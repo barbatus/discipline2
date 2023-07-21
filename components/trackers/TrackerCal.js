5eca5907f1e2132f731145d897a1bb76ba214bab
Aliaksandr <alex.borodach@gmail.com>
2023-07-22 01:26:47 +0200

    Make rates button bigger, render rating out 5 only

--- components/trackers/TrackerCal.js
+++ components/trackers/TrackerCal.js
@@ -77,7 +77,9 @@ export default class TrackerCal extends PureComponent {
     const { trackerType, style, shown } = this.props;
     const { ticksMap } = this.state;
 
-    const showTotal = !(trackerType === TrackerType.STOPWATCH || trackerType === TrackerType.RATE);
+    const showTotal = !(
+      trackerType === TrackerType.STOPWATCH || trackerType === TrackerType.RATE
+    );
     return (
       <Animated.View style={[styles.container, style]}>
         <Calendar
